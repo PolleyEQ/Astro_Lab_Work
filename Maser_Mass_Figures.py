6af6394e43c19c0f02a6324ac89c9f28b50c027d
WaferGOD <157624302+WaferGOD@users.noreply.github.com>
2024-03-26 13:01:58 -0400

    Add files via upload

--- Maser_Mass_Figures.py
+++ Maser_Mass_Figures.py
@@ -38,19 +38,170 @@ def Mass(v):
     
     return mass
 
+
+#%% 3arcseconds
+
 #Import the data, Home computer
-#pr, w1, w2, w3, w4, v_disp = np.genfromtxt('C:/Users/eqpol/OneDrive/Documents/Class Work/GitHub/Astro_Lab_Work/MyTable_CrossDiffCoords_3arcsec_corrected.csv', delimiter=',', skip_header=1, usecols=(1,4,5,6,7, -1), unpack=True)
-#pr_nm, w1_nm, w2_nm, w3_nm, w4_nm, v_disp_nm = np.genfromtxt('C:/Users/eqpol/OneDrive/Documents/Class Work/Github/Astro_Lab_Work/MyTable_CrossDiffCoords_3arcsec_NM.csv', delimiter=',', skip_header=1, usecols=(1,4,5,6,7, -1), unpack=True)
+pr, w1, w2, w3, w4, v_disp = np.genfromtxt('C:/Users/eqpol/OneDrive/Documents/Class Work/GitHub/Astro_Lab_Work/MyTable_CrossDiffCoords_3arcsec_corrected.csv', delimiter=',', skip_header=1, usecols=(1,4,5,6,7, -1), unpack=True)
+pr_nm, w1_nm, w2_nm, w3_nm, w4_nm, v_disp_nm = np.genfromtxt('C:/Users/eqpol/OneDrive/Documents/Class Work/Github/Astro_Lab_Work/MyTable_CrossDiffCoords_3arcsec_NM.csv', delimiter=',', skip_header=1, usecols=(1,4,5,6,7, -1), unpack=True)
 #Import the data, Lab computer
-pr, w1, w2, w3, w4, v_disp = np.genfromtxt('/Users/ethanpolley/Downloads/MyTable_CrossDiffCoords_3arcsec_corrected_POLLEYEQ.csv', delimiter=',', skip_header=1, usecols=(1,4,5,6,7, -1), unpack=True)
-pr_nm, w1_nm, w2_nm, w3_nm, w4_nm, v_disp_nm = np.genfromtxt('/Users/ethanpolley/Downloads/MyTable_CrossDiffCoords_3arcsec_NM_POLLEYEQ.csv', delimiter=',', skip_header=1, usecols=(1,4,5,6,7, -1), unpack=True)
+#pr, w1, w2, w3, w4, v_disp = np.genfromtxt('/Users/ethanpolley/Downloads/MyTable_CrossDiffCoords_3arcsec_corrected_POLLEYEQ.csv', delimiter=',', skip_header=1, usecols=(1,4,5,6,7, -1), unpack=True)
+#pr_nm, w1_nm, w2_nm, w3_nm, w4_nm, v_disp_nm = np.genfromtxt('/Users/ethanpolley/Downloads/MyTable_CrossDiffCoords_3arcsec_NM_POLLEYEQ.csv', delimiter=',', skip_header=1, usecols=(1,4,5,6,7, -1), unpack=True)
 
 #Iterate through the objects to creeate a list of masses
 masses_3m = []
 masses_3nm = []
-n = 0
-for n in range(len(v_disp)):
-    masses_3m.append(Mass(v_disp[n])) #Added each mass to the list
-    masses_3nm.append(Mass(v_disp_nm[n]))
+for n in range(len(v_disp)): #Iterate over all velocities for masers
+    masses_3m.append(Mass(v_disp[n])) #Added each mass to the maser list
+for n in range(len(v_disp_nm)): #Iterate over all velocities for non-masers
+    masses_3nm.append(Mass(v_disp_nm[n])) #Add each mass to the non-maser list
+
+print(pr)
+print(masses_3m)
+
+plt.errorbar(pr, masses_3m, fmt = '*', color = 'blue')
+plt.errorbar(pr_nm, masses_3nm, fmt = 'o', color = 'red')
+plt.ylabel('Mass (in solar masses)')
+plt.xlabel('PR')
+plt.legend(['Masers', 'Non-masers'])
+plt.yscale('Log')
 
 
+#%% 4arcseconds
+
+#Import the data, Home computer
+pr, w1, w2, w3, w4, v_disp = np.genfromtxt('C:/Users/eqpol/OneDrive/Documents/Class Work/GitHub/Astro_Lab_Work/MyTable_CrossDiffCoords_3arcsec_corrected.csv', delimiter=',', skip_header=1, usecols=(1,4,5,6,7, -1), unpack=True)
+pr_nm, w1_nm, w2_nm, w3_nm, w4_nm, v_disp_nm = np.genfromtxt('C:/Users/eqpol/OneDrive/Documents/Class Work/Github/Astro_Lab_Work/MyTable_CrossDiffCoords_3arcsec_NM.csv', delimiter=',', skip_header=1, usecols=(1,4,5,6,7, -1), unpack=True)
+#Import the data, Lab computer
+#pr, w1, w2, w3, w4, v_disp = np.genfromtxt('/Users/ethanpolley/Downloads/MyTable_CrossDiffCoords_4arcsec_POLLEYEQ.csv', delimiter=',', skip_header=1, usecols=(1,4,5,6,7, -1), unpack=True)
+#pr_nm, w1_nm, w2_nm, w3_nm, w4_nm, v_disp_nm = np.genfromtxt('/Users/ethanpolley/Downloads/MyTable_CrossDiffCoords_4arcsec_NM_POLLEYEQ.csv', delimiter=',', skip_header=1, usecols=(1,4,5,6,7, -1), unpack=True)
+
+#Iterate through the objects to creeate a list of masses
+masses_m = []
+masses_nm = []
+for n in range(len(v_disp)): #Iterate over all velocities for masers
+    masses_m.append(Mass(v_disp[n])) #Added each mass to the maser list
+for n in range(len(v_disp_nm)): #Iterate over all velocities for non-masers
+    masses_nm.append(Mass(v_disp_nm[n])) #Add each mass to the non-maser list
+
+print(pr)
+print(masses_3m)
+
+plt.errorbar(pr, masses_m, fmt = '*', color = 'blue')
+plt.errorbar(pr_nm, masses_nm, fmt = 'o', color = 'red')
+plt.ylabel('Mass (in solar masses)')
+plt.xlabel('PR')
+plt.legend(['Masers', 'Non-masers'])
+plt.yscale('Log')
+
+
+#%% 5arcseconds
+
+#Import the data, Home computer
+pr, w1, w2, w3, w4, v_disp = np.genfromtxt('C:/Users/eqpol/OneDrive/Documents/Class Work/GitHub/Astro_Lab_Work/MyTable_CrossDiffCoords_3arcsec_corrected.csv', delimiter=',', skip_header=1, usecols=(1,4,5,6,7, -1), unpack=True)
+pr_nm, w1_nm, w2_nm, w3_nm, w4_nm, v_disp_nm = np.genfromtxt('C:/Users/eqpol/OneDrive/Documents/Class Work/Github/Astro_Lab_Work/MyTable_CrossDiffCoords_3arcsec_NM.csv', delimiter=',', skip_header=1, usecols=(1,4,5,6,7, -1), unpack=True)
+#Import the data, Lab computer
+#pr, w1, w2, w3, w4, v_disp = np.genfromtxt('/Users/ethanpolley/Downloads/MyTable_CrossDiffCoords_5arcsec_POLLEYEQ.csv', delimiter=',', skip_header=1, usecols=(1,4,5,6,7, -1), unpack=True)
+#pr_nm, w1_nm, w2_nm, w3_nm, w4_nm, v_disp_nm = np.genfromtxt('/Users/ethanpolley/Downloads/MyTable_CrossDiffCoords_5arcsec_NM_POLLEYEQ.csv', delimiter=',', skip_header=1, usecols=(1,4,5,6,7, -1), unpack=True)
+
+#Iterate through the objects to creeate a list of masses
+masses_m = []
+masses_nm = []
+for n in range(len(v_disp)): #Iterate over all velocities for masers
+    masses_m.append(Mass(v_disp[n])) #Added each mass to the maser list
+for n in range(len(v_disp_nm)): #Iterate over all velocities for non-masers
+    masses_nm.append(Mass(v_disp_nm[n])) #Add each mass to the non-maser list
+
+print(pr)
+print(masses_3m)
+
+plt.errorbar(pr, masses_m, fmt = '*', color = 'blue')
+plt.errorbar(pr_nm, masses_nm, fmt = 'o', color = 'red')
+plt.ylabel('Mass (in solar masses)')
+plt.xlabel('PR')
+plt.legend(['Masers', 'Non-masers'])
+plt.yscale('Log')
+
+
+#%% 6arcseconds
+
+#Import the data, Home computer
+pr, w1, w2, w3, w4, v_disp = np.genfromtxt('C:/Users/eqpol/OneDrive/Documents/Class Work/GitHub/Astro_Lab_Work/MyTable_CrossDiffCoords_3arcsec_corrected.csv', delimiter=',', skip_header=1, usecols=(1,4,5,6,7, -1), unpack=True)
+pr_nm, w1_nm, w2_nm, w3_nm, w4_nm, v_disp_nm = np.genfromtxt('C:/Users/eqpol/OneDrive/Documents/Class Work/Github/Astro_Lab_Work/MyTable_CrossDiffCoords_3arcsec_NM.csv', delimiter=',', skip_header=1, usecols=(1,4,5,6,7, -1), unpack=True)
+#Import the data, Lab computer
+#pr, w1, w2, w3, w4, v_disp = np.genfromtxt('/Users/ethanpolley/Downloads/MyTable_CrossDiffCoords_6arcsec_POLLEYEQ.csv', delimiter=',', skip_header=1, usecols=(1,4,5,6,7, -1), unpack=True)
+#pr_nm, w1_nm, w2_nm, w3_nm, w4_nm, v_disp_nm = np.genfromtxt('/Users/ethanpolley/Downloads/MyTable_CrossDiffCoords_6arcsec_NM_POLLEYEQ.csv', delimiter=',', skip_header=1, usecols=(1,4,5,6,7, -1), unpack=True)
+
+#Iterate through the objects to creeate a list of masses
+masses_m = []
+masses_nm = []
+for n in range(len(v_disp)): #Iterate over all velocities for masers
+    masses_m.append(Mass(v_disp[n])) #Added each mass to the maser list
+for n in range(len(v_disp_nm)): #Iterate over all velocities for non-masers
+    masses_nm.append(Mass(v_disp_nm[n])) #Add each mass to the non-maser list
+
+print(pr)
+print(masses_3m)
+
+plt.errorbar(pr, masses_m, fmt = '*', color = 'blue')
+plt.errorbar(pr_nm, masses_nm, fmt = 'o', color = 'red')
+plt.ylabel('Mass (in solar masses)')
+plt.xlabel('PR')
+plt.legend(['Masers', 'Non-masers'])
+plt.yscale('Log')
+
+
+#%% 7arcseconds
+
+#Import the data, Home computer
+pr, w1, w2, w3, w4, v_disp = np.genfromtxt('C:/Users/eqpol/OneDrive/Documents/Class Work/GitHub/Astro_Lab_Work/MyTable_CrossDiffCoords_3arcsec_corrected.csv', delimiter=',', skip_header=1, usecols=(1,4,5,6,7, -1), unpack=True)
+pr_nm, w1_nm, w2_nm, w3_nm, w4_nm, v_disp_nm = np.genfromtxt('C:/Users/eqpol/OneDrive/Documents/Class Work/Github/Astro_Lab_Work/MyTable_CrossDiffCoords_3arcsec_NM.csv', delimiter=',', skip_header=1, usecols=(1,4,5,6,7, -1), unpack=True)
+#Import the data, Lab computer
+#pr, w1, w2, w3, w4, v_disp = np.genfromtxt('/Users/ethanpolley/Downloads/MyTable_CrossDiffCoords_7arcsec_POLLEYEQ.csv', delimiter=',', skip_header=1, usecols=(1,4,5,6,7, -1), unpack=True)
+#pr_nm, w1_nm, w2_nm, w3_nm, w4_nm, v_disp_nm = np.genfromtxt('/Users/ethanpolley/Downloads/MyTable_CrossDiffCoords_7arcsec_NM_POLLEYEQ.csv', delimiter=',', skip_header=1, usecols=(1,4,5,6,7, -1), unpack=True)
+
+#Iterate through the objects to creeate a list of masses
+masses_m = []
+masses_nm = []
+for n in range(len(v_disp)): #Iterate over all velocities for masers
+    masses_m.append(Mass(v_disp[n])) #Added each mass to the maser list
+for n in range(len(v_disp_nm)): #Iterate over all velocities for non-masers
+    masses_nm.append(Mass(v_disp_nm[n])) #Add each mass to the non-maser list
+
+print(pr)
+print(masses_3m)
+
+plt.errorbar(pr, masses_m, fmt = '*', color = 'blue')
+plt.errorbar(pr_nm, masses_nm, fmt = 'o', color = 'red')
+plt.ylabel('Mass (in solar masses)')
+plt.xlabel('PR')
+plt.legend(['Masers', 'Non-masers'])
+plt.yscale('Log')
+
+
+#%% 8arcseconds
+
+#Import the data, Home computer
+pr, w1, w2, w3, w4, v_disp = np.genfromtxt('C:/Users/eqpol/OneDrive/Documents/Class Work/GitHub/Astro_Lab_Work/MyTable_CrossDiffCoords_3arcsec_corrected.csv', delimiter=',', skip_header=1, usecols=(1,4,5,6,7, -1), unpack=True)
+pr_nm, w1_nm, w2_nm, w3_nm, w4_nm, v_disp_nm = np.genfromtxt('C:/Users/eqpol/OneDrive/Documents/Class Work/Github/Astro_Lab_Work/MyTable_CrossDiffCoords_3arcsec_NM.csv', delimiter=',', skip_header=1, usecols=(1,4,5,6,7, -1), unpack=True)
+#Import the data, Lab computer
+#pr, w1, w2, w3, w4, v_disp = np.genfromtxt('/Users/ethanpolley/Downloads/MyTable_CrossDiffCoords_8arcsec_POLLEYEQ.csv', delimiter=',', skip_header=1, usecols=(1,4,5,6,7, -1), unpack=True)
+#pr_nm, w1_nm, w2_nm, w3_nm, w4_nm, v_disp_nm = np.genfromtxt('/Users/ethanpolley/Downloads/MyTable_CrossDiffCoords_8arcsec_NM_POLLEYEQ.csv', delimiter=',', skip_header=1, usecols=(1,4,5,6,7, -1), unpack=True)
+
+#Iterate through the objects to creeate a list of masses
+masses_m = []
+masses_nm = []
+for n in range(len(v_disp)): #Iterate over all velocities for masers
+    masses_m.append(Mass(v_disp[n])) #Added each mass to the maser list
+for n in range(len(v_disp_nm)): #Iterate over all velocities for non-masers
+    masses_nm.append(Mass(v_disp_nm[n])) #Add each mass to the non-maser list
+
+print(pr)
+print(masses_3m)
+
+plt.errorbar(pr, masses_m, fmt = '*', color = 'blue')
+plt.errorbar(pr_nm, masses_nm, fmt = 'o', color = 'red')
+plt.ylabel('Mass (in solar masses)')
+plt.xlabel('PR')
+plt.legend(['Masers', 'Non-masers'])
+plt.yscale('Log')
